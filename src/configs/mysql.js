fae950eb3ed4f6b7fb206421a5d9cfe4237c7995
hendras <muhsyahendraa1722@gmail.com>
2021-02-17 23:07:33 +0700

    handle Lost Connection

--- src/configs/mysql.js
+++ src/configs/mysql.js
@@ -1,15 +1,36 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+  connectionLimit: 10,
   host: 'us-cdbr-east-03.cleardb.com',
   user: 'bcf464e6efc54a',
   password: '4c5b3b69',
   database: 'heroku_94cb71c6b7eac6b'
 })
 
-connection.connect((error) => {
-  if (error) console.log(error)
-  console.log('Database connected!')
+connection.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
+  if (connection) connection.release()
+
+  return console.log('Database Connected')
 })
+
+// connection.connect((error) => {
+//   if (error) {
+//     console.log(error)
+//   }
+//   console.log('Database connected!')
+// })
+
 
 module.exports = connection
